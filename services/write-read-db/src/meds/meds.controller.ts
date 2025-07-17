61d4e447b3dc4a2b084883dbc7ecb96d499a9b52
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2025-07-17 16:50:17 -0400

    test

--- services/write-read-db/src/meds/meds.controller.ts
+++ services/write-read-db/src/meds/meds.controller.ts
@@ -4,4 +4,5 @@ import { MedsService } from './meds.service';
 @Controller('meds')
 export class MedsController {
   constructor(private readonly medsService: MedsService) {}
+  // Define your endpoints here, e.g.:
 }
